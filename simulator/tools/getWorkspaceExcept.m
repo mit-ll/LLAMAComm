fae8040abb5f58ebbb3729f671c3ba534486a756
Glenn Fawcett <fawcett@ll.mit.edu>
2016-09-26 12:11:12 -0400

    Added missing copyright notices

--- simulator/tools/getWorkspaceExcept.m
+++ simulator/tools/getWorkspaceExcept.m
@@ -12,10 +12,36 @@ function out = getWorkspaceExcept(varargin)
 % structure, provided they do not occur in the provided list of exceptions.
 %
 %
-%Author: G.S. Fawcett
-%Original date: 20/Apr/2016
-%Last modified: 20/Apr/2016
+
+% Copyright (c) 2006-2016, Massachusetts Institute of Technology All rights
+% reserved.
 %
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+%      * Redistributions of source code must retain the above copyright
+%        notice, this list of conditions and the following disclaimer.
+%      * Redistributions in binary form must reproduce the above  copyright
+%        notice, this list of conditions and the following disclaimer in
+%        the documentation and/or other materials provided with the
+%        distribution.
+%      * Neither the name of the Massachusetts Institute of Technology nor
+%        the names of its contributors may be used to endorse or promote
+%        products derived from this software without specific prior written
+%        permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+% IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+% THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+% PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+% CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+% EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+% PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+% PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 if nargin == 1 && iscell(varargin{1})
   exceptions = varargin{1};
 else
@@ -33,3 +59,31 @@ if ~isempty(wh)
   end
 end
 
+% Copyright (c) 2006-2016, Massachusetts Institute of Technology All rights
+% reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+%      * Redistributions of source code must retain the above copyright
+%        notice, this list of conditions and the following disclaimer.
+%      * Redistributions in binary form must reproduce the above  copyright
+%        notice, this list of conditions and the following disclaimer in
+%        the documentation and/or other materials provided with the
+%        distribution.
+%      * Neither the name of the Massachusetts Institute of Technology nor
+%        the names of its contributors may be used to endorse or promote
+%        products derived from this software without specific prior written
+%        permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+% IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+% THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+% PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+% CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+% EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+% PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+% PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
