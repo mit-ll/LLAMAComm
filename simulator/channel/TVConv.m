ac7799b7ecca96ec46ccde38b2c0fee0b2a8b0c8
Glenn Fawcett <fawcett@ll.mit.edu>
2019-06-04 16:33:53 -0400

    M-file version of TVConv actually works now

--- simulator/channel/TVConv.m
+++ simulator/channel/TVConv.m
@@ -1,23 +1,20 @@
-function temp = TVConv(H, powerProfileLags, src, longestLag)
-% DISTRIBUTION STATEMENT A. Approved for public release.
-% Distribution is unlimited.
-%
-% This material is based upon work supported by the Defense Advanced Research
-% Projects Agency under Air Force Contract No. FA8702-15-D-0001. Any opinions,
-% findings, conclusions or recommendations expressed in this material are those
-% of the author(s) and do not necessarily reflect the views of the Defense
+function output = TVConv(H, lags, src, longestLag)
+% Approved for public release: distribution unlimited.
+% 
+% This material is based upon work supported by the Defense Advanced Research 
+% Projects Agency under Air Force Contract No. FA8721-05-C-0002. Any opinions, 
+% findings, conclusions or recommendations expressed in this material are those 
+% of the author(s) and do not necessarily reflect the views of the Defense 
 % Advanced Research Projects Agency.
-%
-% © 2019 Massachusetts Institute of Technology.
-%
-% Subject to FAR52.227-11 Patent Rights - Ownership by the contractor (May 2014)
-%
+% 
+% © 2014 Massachusetts Institute of Technology.
+% 
 % The software/firmware is provided to you on an As-Is basis
-%
-% Delivered to the U.S. Government with Unlimited Rights, as defined in DFARS
-% Part 252.227-7013 or 7014 (Feb 2014). Notwithstanding any copyright notice,
-% U.S. Government rights in this work are defined by DFARS 252.227-7013 or
-% DFARS 252.227-7014 as detailed above. Use of this work other than as
+% 
+% Delivered to the U.S. Government with Unlimited Rights, as defined in DFARS 
+% Part 252.227-7013 or 7014 (Feb 2014). Notwithstanding any copyright notice, 
+% U.S. Government rights in this work are defined by DFARS 252.227-7013 or 
+% DFARS 252.227-7014 as detailed above. Use of this work other than as 
 % specifically authorized by the U.S. Government may violate any copyrights
 % that exist in this work.
 
@@ -26,37 +23,38 @@ persistent calledBefore
 if isempty(calledBefore)
   fprintf(1, ['\n   WARNING Missing MEX function: TVConv.%s',  ...
               '.\n   You can create the mex function by changing', ...
-              ' the\n   working directory to', ...
-              ' /simulator/channel/\n   and typing "mex', ...
-              ' TVConv.c"\n\n'], mexext);
-  calledBefore = true;
+              ' the\n   working directory to', ...                    
+              ' /simulator/channel/\n   and typing "mex', ...         
+              ' TVConv.c"\n\n'], mexext);                             
+  calledBefore = true;                                                
+end                                                                   
+
+frmLen = size(H, 1); % obtain the number of samples
+%%numLags = size(H, 2); % obtain the number of lags
+multAddIndices = longestLag - lags + 1; % compute indices in signal buffer corresponding to non-zero channel filter tap values
+Hf = flipud(H); % convolutional flip of filter taps
+
+innerProduct = zeros(1, frmLen);
+
+for n = 1:min(frmLen, longestLag) % loop through the sample index
+    firstIndx = 1;
+    sigBuffer = src(firstIndx:n).';
+    
+    sigFilt = Hf(n, :); % obtain the current channel taps
+    culledIndices = multAddIndices <= length(sigBuffer); % obtain the indices over which the multiply-add is to be performed
+    innerProduct(n) = sum(sigFilt(culledIndices).*sigBuffer(multAddIndices(culledIndices))); % perform the multiply-add   
+end
+for n = longestLag+1:frmLen % loop through the sample index
+    firstIndx = n - longestLag;    
+    sigBuffer = src(firstIndx:n).';
+    
+    sigFilt = Hf(n, :); % obtain the current channel taps
+    culledIndices = multAddIndices <= length(sigBuffer); % obtain the indices over which the multiply-add is to be performed
+    innerProduct(n) = sum(sigFilt(culledIndices).*sigBuffer(multAddIndices(culledIndices))); % perform the multiply-add   
 end
 
-temp = zeros(1, nS);
-lags = fliplr(longestLag - powerProfileLags);
-for sLoop = 1:nS
-  temp(sLoop) = H(sLoop, inds)*src(lags + sLoop);
-end
 
-% DISTRIBUTION STATEMENT A. Approved for public release.
-% Distribution is unlimited.
-%
-% This material is based upon work supported by the Defense Advanced Research
-% Projects Agency under Air Force Contract No. FA8702-15-D-0001. Any opinions,
-% findings, conclusions or recommendations expressed in this material are those
-% of the author(s) and do not necessarily reflect the views of the Defense
-% Advanced Research Projects Agency.
-%
-% © 2019 Massachusetts Institute of Technology.
-%
-% Subject to FAR52.227-11 Patent Rights - Ownership by the contractor (May 2014)
-%
-% The software/firmware is provided to you on an As-Is basis
-%
-% Delivered to the U.S. Government with Unlimited Rights, as defined in DFARS
-% Part 252.227-7013 or 7014 (Feb 2014). Notwithstanding any copyright notice,
-% U.S. Government rights in this work are defined by DFARS 252.227-7013 or
-% DFARS 252.227-7014 as detailed above. Use of this work other than as
-% specifically authorized by the U.S. Government may violate any copyrights
-% that exist in this work.
-
+% obtain the "valid" convolution output by taking off the first longestLag samples 
+output = zeros(size(innerProduct));
+temp = innerProduct(longestLag+1:end);
+output(1:length(temp)) = temp;
